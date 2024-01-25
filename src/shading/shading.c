7acdf04c81807862dcc6ab6d03154518e6902ec6
Felicia Koolhoven <fmkoolhoven@gmail.com>
2024-01-25 10:52:48 +0100

    Working on ambient light

--- src/shading/shading.c
+++ src/shading/shading.c
@@ -5,13 +5,20 @@
 // If the color is white and ratio 1.0 should everything be entirely white?
 t_color apply_shading(t_data *data, t_color *object_color)
 {
+	// change object color based on light sources
+
+
+
 	t_ambient_light	ambient_light;
 	t_color			final_color;
 
 	ambient_light = data->ambient;
 	ambient_light.color = multiply(&ambient_light.color, ambient_light.ratio);
-	ambient_light.color = divide(&ambient_light.color, 255);
-	*object_color = divide(object_color, 255);
-	final_color = vector_multiply(object_color, &ambient_light.color);
+	ambient_light.color = divide(&ambient_light.color, 255.0);
+	*object_color = divide(object_color, 255.0);
+	final_color = vector_add(object_color, &ambient_light.color);
+	final_color.x = fmin(1.0, final_color.x);
+	final_color.y = fmin(1.0, final_color.y);
+	final_color.z = fmin(1.0, final_color.z);
     return (final_color);
 }
